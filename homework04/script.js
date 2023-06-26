42b40161c59ff8209e3ce2d3a3e8dad2382c82ac
Olena <lena.payuk@gmail.com>
2023-06-26 09:25:39 +0300

    add i conjunction to pairs

--- homework04/script.js
+++ homework04/script.js
@@ -7,52 +7,51 @@ const themes = [
 const marks = [4, 5, 5, 3, 4, 5];
 
 //TASK#1
-function getPairs (students) {
+function getPairs(students) {
   const men = ["Олександр", "Ігор", "Олексій"];
-    const women = ["Олена", "Іра", "Світлана"];
-    const result1 = [];
-    for (let i = 0; i < men.length; i++) {
-      if (students.includes(men[i]) && students.includes(women[i])) {
-       result1.push([men[i] +' i '+ women[i]]);
-      }
+  const women = ["Олена", "Іра", "Світлана"];
+  const result1 = [];
+  for (let i = 0; i < men.length; i++) {
+    if (students.includes(men[i]) && students.includes(women[i])) {
+      result1.push([`${men[i]} i ${women[i]}`]);
     }
-    return result1;
   }
-  const pairs = getPairs (students);
-  console.log('Сформовані пари:', pairs);
-  
+  return result1;
+}
+const pairs = getPairs(students);
+console.log("Сформовані пари:", pairs);
 
- //TASK#2
- function getThemes (pairs, themes) { 
- const result2 = [];
+//TASK#2
+function getThemes(pairs, themes) {
+  const result2 = [];
   for (let i = 0; i < pairs.length; i++) {
-   result2.push(pairs[i].concat(themes[i]));
+    result2.push(pairs[i].concat(themes[i]));
   }
   return result2;
 }
-const themesForPairs = getThemes (pairs, themes)
-console.log('Теми для пар:', themesForPairs);
+const themesForPairs = getThemes(pairs, themes);
+console.log("Теми для пар:", themesForPairs);
 
 //TASK#3
-function getMark (students, marks) {
-let result3 = [];
-for (let i = 0; i < students.length; i++) {
-  result3.push([students[i], marks[i]]);
+function getMark(students, marks) {
+  let result3 = [];
+  for (let i = 0; i < students.length; i++) {
+    result3.push([students[i], marks[i]]);
+  }
+  return result3;
 }
-return result3;
-}
-const marksForStudents = getMark (students, marks);
-console.log('Оцінка студента:',marksForStudents);
+const marksForStudents = getMark(students, marks);
+console.log("Оцінка студента:", marksForStudents);
 
 //TASK#4
-function getMarkForPairs (themesForPairs) {
-let result4 = [];
-for (let i = 0; i < themesForPairs.length; i++) {
-  result4.push([
-    themesForPairs[i].concat(Math.round(Math.random() * (5 - 1) + 1)),
-  ]);
+function getMarkForPairs(themesForPairs) {
+  let result4 = [];
+  for (let i = 0; i < themesForPairs.length; i++) {
+    result4.push([
+      themesForPairs[i].concat(Math.round(Math.random() * (5 - 1) + 1)),
+    ]);
+  }
+  return result4;
 }
-return result4;
-}
-const randomMarksForPairs = getMarkForPairs (themesForPairs);
-console.log('Оцінка пари:', randomMarksForPairs);
+const randomMarksForPairs = getMarkForPairs(themesForPairs);
+console.log("Оцінка пари:", randomMarksForPairs);
