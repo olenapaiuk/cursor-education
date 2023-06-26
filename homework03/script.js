ba108f3d25fe9de3aa7af31d45c61db37567c378
Olena <lena.payuk@gmail.com>
2023-06-26 11:16:20 +0300

    after II code refactoring

--- homework03/script.js
+++ homework03/script.js
@@ -1,7 +1,8 @@
 // TASK#1
 alert("Завдання№1");
-function getMaxDigit(number = prompt("Введіть число:", "")) {
-  const unit = String(number).split("");
+function getMaxDigit() {
+  let num = prompt("Введіть число:", "");
+  const unit = String(num).split("");
   let unitMax = unit[0];
   for (let i = 0; i < unit.length; i++) {
     if (unit[i] > unitMax) {
@@ -14,29 +15,36 @@ console.log("1. Mаксимальне значення числа станови
 
 // TASK#2
 alert("Ви переходете до завдання№2");
-function getNumberExponent(num = prompt("Введіть число num яке хочете піднести до степеня:", ""),
-  exp = prompt("Введіть степінь exp:", "")) {
+function getNumberExponent(num, exp) {
+  num = prompt("Введіть число num яке хочете піднести до степеня:", "");
+  exp = prompt("Введіть степінь exp:", "");
   let result2 = 1;
   for (let i = 0; i < exp; i++) {
     result2 = result2 * num;
   }
   return result2;
 }
-console.log("2. Значення числа num піднесеного до степеня exp:", getNumberExponent());
+console.log(
+  "2. Значення числа num піднесеного до степеня exp:",
+  getNumberExponent()
+);
 
 // TASK#3
 alert("Завдання№3. Запишемо ім'я з великої літери");
-function capitalLetter(name = prompt("Вкажіть значення імені:", "")) {
+function capitalLetter() {
+  const name = prompt("Вкажіть значення імені:", "");
   const nameToLowerCase = name.toLowerCase();
   let result3 = "";
-      result3 = nameToLowerCase[0].toUpperCase() + nameToLowerCase.slice(1);
-      return result3;
-    }
+  result3 = nameToLowerCase[0].toUpperCase() + nameToLowerCase.slice(1);
+  return result3;
+}
 console.log("3. Коректне написання імені:", capitalLetter());
 
 //TASK#5
 alert("Завдання№5. Повернення випадкового числа");
-function getRandomNumber(N = prompt("Введіть перше число:", ""), M = prompt("Введіть друге число:", "")) {
+function getRandomNumber(N, M) {
+  N = prompt("Введіть перше число:", "");
+  M = prompt("Введіть друге число:", "");
   const NToNum = Number(N);
   const MToNum = Number(M);
 
@@ -50,10 +58,12 @@ console.log(`5. Випадкове число:`, getRandomNumber());
 
 //TASK#6
 alert("Завдання№6. Обрахунок повторювальних літер");
-function countLetter(word = prompt("Введіть слово:", ""),  letter = prompt("Введіть літеру:", "")) {
+function countLetter(word1, letter1) {
+  word1 = prompt("Введіть слово:", "");
+  letter1 = prompt("Введіть літеру:", "");
   let result6 = 0;
-  for (let i = 0; i <= word.length; i++) {
-    if (letter == word[i]) {
+  for (let i = 0; i <= word1.length; i++) {
+    if (letter1 == word1[i]) {
       result6 += 1;
     }
   }
@@ -63,12 +73,14 @@ console.log("6. Кількість повторювальних літер:", co
 
 //TASK#10
 alert("Завдання№10. Видалення літери зі слова");
-function deleteLetter(word = prompt("Введіть слово:", ""), letter = prompt("Введіть літеру:", "")) {
+function deleteLetter(word2, letter2) {
+  word2 = prompt("Введіть слово:", "");
+  letter2 = prompt("Введіть літеру:", "");
   let result10 = "";
 
-  for (let i = 0; i < word.length; i++) {
-    if (letter !== word[i]) {
-      result10 = result10 + word[i];
+  for (let i = 0; i < word2.length; i++) {
+    if (letter2 !== word2[i]) {
+      result10 = result10 + word2[i];
     }
   }
   return result10;
@@ -77,18 +89,19 @@ console.log("10. Слово після видалення літер:", deleteLe
 
 //TASK#11
 alert("Завдання№11. Перевірка на паліндромність");
-function isPallindrom(word = prompt("Введіть слово:", "")) {
-  const wordToLowerCase = word.toLowerCase();
+function isPallindrom(word3) {
+  word3 = prompt("Введіть слово:", "");
+  const wordToLowerCase = word3.toLowerCase();
   let reversedWord = "";
 
   for (let i = wordToLowerCase.length - 1; i >= 0; i--) {
     reversedWord += wordToLowerCase[i];
   }
 
-    if (wordToLowerCase == reversedWord) {
-      return true;
-    } else {
-      return false;
+  if (wordToLowerCase == reversedWord) {
+    return true;
+  } else {
+    return false;
   }
 }
 console.log("11. Чи є слово паліндромом?:", isPallindrom());
