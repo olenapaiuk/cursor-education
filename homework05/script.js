366a660d97fef5bbbfdb86f7211d8b81ca643dfc
Olena <lena.payuk@gmail.com>
2023-07-04 13:49:05 +0300

    task after refactoring

--- homework05/script.js
+++ homework05/script.js
@@ -20,33 +20,33 @@ const result1 = getRandomArray();
 console.log('1. Отриманий масив випадкових чисел:', result1);
 
 
-//TASK#3
-function getAverage(...numbers) {
-  numbers = prompt("Завдання 3. Введіть набір ЦІЛИХ чисел через пробіл:", "");
+//FUNCTION CHECK for tasks 3,5-7
+function checkString (numbers) {
+  numbers = prompt("Введіть набір ЦІЛИХ чисел через пробіл. Цей набір чисел використовується при вирішенні завдань 3, 5-7:", "");
   const array = numbers.split(' ');
   array.forEach(value => {
     if (!Number.isInteger(+value) || isNaN(+value) ||!(+value) || value.trim() == '') 
           {return numbers = prompt("Введіть КОРЕКТНО набір ЦІЛИХ чисел через пробіл:", "")}
   });
-  let array1=numbers.split(' ').map(value=>+value);
+  return numbers;
+}
+const checkedString = checkString();
+
+//TASK#3
+function getAverage() {
+  let array1=checkedString.split(' ').map(value=>+value);
   const sum = array1.reduce(function (acc, el) {
     return acc + (el);
   });
-  const average = Math.round(sum / array.length);
+  const average = Math.round(sum / checkedString.length);
   return average;
 }
 const result3 = getAverage();
 console.log("3. Отримане середнє значення:", result3);
 
 //TASK#5
-function filterEvenNumbers (numbers) {
-  numbers = prompt("Завдання 5. Введіть масив ЦІЛИХ чисел через пробіл:", "");
-  const array = numbers.split(' ');
-  array.forEach(value => {
-    if (!Number.isInteger(+value) || isNaN(+value) ||!(+value) || value.trim() == '') 
-          {return numbers = prompt("Введіть КОРЕКТНО набір ЦІЛИХ чисел через пробіл:", "")}
-  });
-const x = numbers.split(' ');
+function filterEvenNumbers () {
+const x = checkedString.split(' ');
 const filteredNumbers = x.map(function (value) {return +value}).filter(function (currentValue) {return currentValue % 2!==0});
   return filteredNumbers;
 }
@@ -55,13 +55,7 @@ console.log('5. Масив без парних значень:', result5);
 
 //TASK#6
 function countPositiveNumbers() {
-  let numbers = prompt("6. Введіть масив чисел через пробіл:", "");
-let array = numbers.split(' ');
-  array.forEach(value => {
-    if (!Number.isInteger(+value) || isNaN(+value) ||!(+value) || value.trim() == '') 
-          {return numbers = prompt("Введіть КОРЕКТНО набір ЦІЛИХ чисел через пробіл:", "")}
-  });
-  const sum = numbers
+  const sum = checkedString
     .split(" ")
     .map(function (value) {
       return +value;
@@ -78,14 +72,8 @@ const result6 = countPositiveNumbers();
 console.log("6.Сума додатніх чисел становить:", result6);
 
 //TASK#7
-function getDividedByFive(...numbers) {
-  numbers = prompt('7. Введіть масив чисел через пробіл:', '');
-  let x = numbers.split(' ');
-  x.forEach(value => {
-    if (!Number.isInteger(+value) || isNaN(+value) ||!(+value) || value.trim() == '') 
-          {return numbers = prompt("Введіть КОРЕКТНО набір ЦІЛИХ чисел через пробіл:", "")}
-  });
-  const array = numbers.split(' ').map(function (value) {
+function getDividedByFive() {
+  const array = checkedString.split(' ').map(function (value) {
     return +value;
   })
   .filter(function (value) {
@@ -96,14 +84,20 @@ return array;
 const result7 = getDividedByFive();
 console.log("7.Масив чисел, що діляться на 5:", result7);
 
-
-//TASK#10
-function generateCombinations(word) {
-  word=prompt('10. Введіть слово для паролю з кількістю літер до 10:', '').toLowerCase();
-  if (word.length >10 || word.length == 0) { word=prompt('Слово не підходить, спробуйте ввести знову:', '')}
-  let array=[];
-  for (let i=0; i<word.length; i++) {array.push([word.slice(i)+word.slice(0, i)])}
-return array;
+//TASK#8
+function replaceBadWords (string) {
+  string = prompt('Введіть речення з нецензурним словом:', '');
+  const stringToArray = string.split(' ');
+  let libraryOfBadWords = ['fuck', 'shit'];
+  let changedArray = [];
+  
+  for (let i=0; i<stringToArray.length; i++) {
+      libraryOfBadWords.forEach(function (value) {if
+      (stringToArray[i].includes(value)) {return changedArray.push(stringToArray[i].replace(value, '****'))}
+  })
+  changedArray.push(stringToArray[i])} //я не знаю як ввести перевірку для того, щоб замінити зміненим словом з масиву незмінене
+  let finalString = changedArray.join(' ');
+  return finalString;
 }
-const result10 = generateCombinations();
-console.log('10. Можливі перестановки слова:', result10);
+const result8 = replaceBadWords();
+console.log('8. Змінена фраза з нецензурним словом:', result8);
