f6ef100534c491342dd75f3d18320b460f64c8f0
olenapaiuk <116064379+olenapaiuk@users.noreply.github.com>
2023-07-18 11:58:53 +0300

    first commit

---
+++ homework07/script.js
@@ -0,0 +1,38 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+//TASK#1
+function getMyTaxes (salary) {
+    tax = Number(salary)*this.tax;
+    return tax;
+}
+const result1 = getMyTaxes.call(ukraine, 3000);
+console.log(`1. Tax in your country is ${result1}`);
+
+//TASK#2
+function getMiddleTaxes () {
+    let middleTax = (this.middleSalary*this.tax).toFixed(2);
+    return middleTax;
+}
+const result2 = getMiddleTaxes.call(ukraine);
+console.log(`2. Middle tax in your country is ${result2}`);
+
+//TASK#3
+function getTotalTaxes () {
+    let totalTaxes = this.tax * this.middleSalary * this.vacancies;
+    return totalTaxes;
+}
+const result3 = getTotalTaxes.call(ukraine);
+console.log(`3. Total taxes in your country are ${result3}`);
+
+//TASK#4
+function getMySalary () {
+        const salary = Math.ceil(Math.random()*(2000-1500)+1500);
+const taxes = getMyTaxes.call(ukraine, salary);
+const profit= salary - taxes;
+return {salary, taxes, profit}
+}
+
+setInterval (() =>  
+    console.log('4. My salary is:', getMySalary(ukraine)), 10000);
